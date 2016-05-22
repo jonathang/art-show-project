be120f057a324b2486763a64a87011fa535f6b0a
Yoni Goldberg <yoni@evenfinancial.com>
2016-05-22 13:51:17 -0400

    improvments to the morse processing

--- app.js
+++ app.js
@@ -6,42 +6,70 @@ var Repeat = require('repeat');
 var _ = require('underscore');
 var sleep = require('sleep');
 
-var GPIO = require('onoff').Gpio;
-var led = new GPIO(18, 'out');
+var DEBUG = false;
 
-var SLEEP_PERIOD_MSEC = 50000;
+if (DEBUG) {
+  var led = {
+    writeSync : function(value) {
+      if (value === 0) {
+        console.log("turn off");
+      } else {
+        console.log("turn on");
+      }
+    }
+  }
+} else {
+  var GPIO = require('onoff').Gpio;
+  var led = new GPIO(18, 'out');
+}
+
+var SLEEP_PERIOD_MSEC = 500000 ;
 var USER_TO_FOLLOW = 'KimKardashian';
 
 var handleTweet = function()  {
   twitter.getLastTweet(USER_TO_FOLLOW, function(tweet) {
-    console.log('text: ' + tweet.text);
-    var morseText = morse.encode(tweet.text).split('');
-    console.log('morse : ' + morseText);
-    _.each(morseText, function(morseChar) {
 
-      switch (morseChar) {
-        case '?':
+    console.log('Tweet Text: ' + tweet.text);
+
+    var morseText = morse.encode(tweet.text);
+    console.log('Morse Text: ' + morseText);
+    var morseLetters = morseText.split(' ');
+    console.log('Morse Letters Array: ' + morseLetters);
+
+    _.each(morseLetters, function(morseLetter) {
+      console.log('Morse Letter: ' + morseLetter);
+      led.writeSync(0);
+      if (morseLetter === '.......') {
+        console.log("Word Space sleep for 4 units")
+        sleep.usleep(SLEEP_PERIOD_MSEC * 4);
+      } else {
+        //Space between letters
+        console.log("Letter Space sleep for 2 units")
+        sleep.usleep(SLEEP_PERIOD_MSEC * 2);
+        var morseChars = morseLetter.split('');
+        _.each(morseChars, function(morseChar) {
+          console.log("Char Space sleep for 1 unit");
           led.writeSync(0);
           sleep.usleep(SLEEP_PERIOD_MSEC);
-          break;
-        case ' ':
-          led.writeSync(1);
-          sleep.usleep(SLEEP_PERIOD_MSEC);
-          break;
-        case '.':
-          led.writeSync(1);
-          sleep.usleep(SLEEP_PERIOD_MSEC);
-          led.writeSync(0);
-          break;
-        case '-':
-          led.writeSync(1);
-          sleep.usleep(SLEEP_PERIOD_MSEC * 3);
-          led.writeSync(0);
-          break;
+          console.log('Morse Char: ' + morseChar);
+          switch (morseChar) {
+            case '.':
+              led.writeSync(1);
+              sleep.usleep(SLEEP_PERIOD_MSEC);
+              break;
+            case '-':
+              led.writeSync(1);
+              sleep.usleep(SLEEP_PERIOD_MSEC * 3);
+              break;
+            default:
+              console.log("Unknown morse character" + morseChar)
+              break;
+            }
+        });
       }
     });
     console.log("finished");
   });
 };
 
-Repeat(handleTweet).every(1000, 'ms').for(1000000, 'hours').start.in(2, 'sec');
+Repeat(handleTweet).every(1, 'minute').for(1000000, 'hours').start.in(2, 'sec');
